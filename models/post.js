b9d1f3f89b4f32a7fcfb8d29d9c5a7940079823c
Prisecaru Emilia <prisecaru.emilia@gmail.com>
2021-06-22 22:02:43 +0300

    modify PostSchema

--- models/post.js
+++ models/post.js
@@ -3,14 +3,26 @@ const mongoose = require('mongoose')
 // const { Schema } = mongoose;
 
   const PostSchema = new mongoose.Schema({
-    title: {
+    author: {
         type: String,
         required: true
+    },
+    time: {
+      type: String,
+      required: true
     },
     content: {
         type: String,
         required: true
-    }
+    },
+    picture: {
+      type: String,
+      required: false
+    },
+    like: {
+      type: Boolean,
+      required: true
+    },
   });
 
   const Post = new mongoose.model('Post', PostSchema);
